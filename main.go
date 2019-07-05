08b92105aeb1946a4ae0c9680037bf1a3d04c98d
ameena3 <anubhav.meena1991@gmail.com>
2019-07-04 20:57:47 -0500

    added test url

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", DefaultHandler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/test", Test)
+	fmt.Println("Listening at port 80")
+	http.ListenAndServe(":80", nil)
 }
 
 //DefaultHandler ...
 func DefaultHandler(w http.ResponseWriter, r *http.Request) {
-
 	fmt.Fprintf(w, "Hey there Anubhav \n")
+	fmt.Println("Default endpoint hit")
+}
+
+//Test ...
+func Test(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Test endpoint hit")
 }
