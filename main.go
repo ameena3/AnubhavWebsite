ac059ea3c319b51c4edb97fe67d1cdd29c399a41
ameena3 <anubhav.meena1991@gmail.com>
2019-07-06 08:18:24 -0500

    test

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", DefaultHandler)
 	http.HandleFunc("/test", Test)
-	fmt.Println("Listening at port 80")
-	http.ListenAndServe(":80", nil)
+	http.Handle("/custom", &Myhandler{greeting: "Anubhav"})
+	http.Handle("/custom1", http.TimeoutHandler(&Myhandler{greeting: "Anubhav"}, time.Second, "hey there timeout"))
+	fmt.Println("Listening at port 8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 //DefaultHandler ...
@@ -21,4 +24,14 @@ func DefaultHandler(w http.ResponseWriter, r *http.Request) {
 //Test ...
 func Test(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test endpoint hit")
+}
+
+//Myhandler test
+type Myhandler struct {
+	greeting string
+}
+
+func (mh *Myhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("%v hello", mh.greeting)))
+	time.Sleep(4 * time.Second)
 }
