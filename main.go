8ab361020a295711396959c5b8bbb1bf59c5783c
ameena3 <anubhav.meena1991@gmail.com>
2020-11-08 15:28:34 -0600

    changing the port

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	http.HandleFunc("/control", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "This is the control.")
 	})
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(":8080", nil)
 	catcherror(err)
 
 }
